d824e9c5e2bc59cda0a5a82fd33b2bd012f07066
Thái <62926491+nhthai173@users.noreply.github.com>
2024-10-16 08:48:16 +0700

    Update RARM.py and setup.py

--- JETSON/RARM.py
+++ JETSON/RARM.py
@@ -1,21 +1,24 @@
 from serial import Serial, PARITY_NONE, STOPBITS_ONE, EIGHTBITS
+from time import sleep
 
 class RARM:
-    def __init__(self, channel = ['9', '12', '17', '21', '24', '28'], port='/dev/ttyACM0'):
+    lastPos = [0, 0, 0, 0, 0, 0]
+    def __init__(self, channel = ['9', '12', '17', '25', '26', '28'], port='/dev/ttyACM0'):
         """Initialize 6 DoF Robot arm
 
         Args:
             port (str, optional): Defaults to '/dev/ttyACM0' for jetson(ubuntu) or 'COMx' for windows.
-            channel (list, optional): Defaults to ['9', '12', '17', '21', '24', '28'].
+            channel (list, optional): Defaults to ['9', '12', '17', '25', '26', '28'].
         """
         self.port = port
         self.channel = channel
         self.serial = Serial(port = self.port, baudrate = 115200, parity = PARITY_NONE, stopbits = STOPBITS_ONE, bytesize = EIGHTBITS, timeout = 1)
-    def setPos(self, pos):
+    def setPos(self, pos, time = 500, delay = 500):
         buf = ''
         for i in range(len(pos)):
+            self.lastPos[i] = pos[i]
             buf += f'#{self.channel[i]}P{pos[i]}'
-        buf+= 'T500D500\r\n'
+        buf+= f'T{time}D{delay}\r\n'
         self.serial.write(buf.encode())
-    def searchPoint(self):
-        pass
+        sleep(time/1000)
+        sleep(1)
